ccce2ab96944274ee3564c645d2f7cd8e56b511c
Dmytro <dmytro.malanchuk@elev.ga.ntig.se>
2024-08-21 10:45:41 +0200

    done

--- main.py
+++ main.py
@@ -3,31 +3,12 @@
 # Datum:2024-08-21
 
 def main():
-    # Läs in input från användaren
-    input_str = input("Ange antalet sidor på de två tärningarna, separerade med ett mellanslag: ")
-    N, M = map(int, input_str.split())
+    user_input = [int(x) for x in input("").split(" ")]
+    min_dice = min(user_input)
+    max_dice = max(user_input)
 
-    # Skapa en dictionary för att räkna hur många gånger varje summa förekommer
-    sum_counts = {}
-
-    # Loopar över alla möjliga tärningskast
-    for i in range(1, N + 1):
-        for j in range(1, M + 1):
-            current_sum = i + j
-            if current_sum in sum_counts:
-                sum_counts[current_sum] += 1
-            else:
-                sum_counts[current_sum] = 1
-
-    # Hitta den högsta frekvensen
-    max_frequency = max(sum_counts.values())
-
-    # Hitta alla summor med den högsta frekvensen
-    most_likely_sums = [s for s in sum_counts if sum_counts[s] == max_frequency]
-
-    # Skriv ut summorna i fallande ordning
-    for sum_value in sorted(most_likely_sums, reverse=True):
-        print(sum_value)
+    for sum in range(min_dice + 1, max_dice + 2):
+        print(sum)
 
 if __name__ == "__main__":
     main()
